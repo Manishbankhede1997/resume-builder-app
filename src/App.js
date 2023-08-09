a0e51334469d678e2ae169ab3a16e1dd9463d316
Manishbankhede1997 <manishbankhede2727@gmail.com>
2023-08-09 17:16:28 +0530

    removed unncessary code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,3 @@
-// import "./App.css";
-// import { BrowserRouter, Routes, Route } from "react-router-dom";
-// import Detailpage from "./pages/DetailPage/Detailpage";
-// import Navbar from "./components/Navbar/Navbar";
-// import Homepage from "./pages/Homepage/Homepage";
-// import About from "./pages/AboutPage/About";
-// function App() {
-//   return (
-//     <div>
-//       <BrowserRouter>
-//         <Navbar />
-//         <Routes>
-//           <Route path="/" element={<Homepage />} />
-//           <Route path="/detailPage" element={<Detailpage />} />
-//           <Route path="/aboutPage" element={<About />} />
-//         </Routes>
-//       </BrowserRouter>
-//     </div>
-//   );
-// }
-
-// export default App;
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Provider } from "react-redux"; // Import Provider
